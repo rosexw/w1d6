75ece4cd2f004d88d750565db3d0bcaefaa28aed
Rose Wan <rwan@mail.com>
2017-06-05 00:16:20 +0000

    step 6

--- https_example.js
+++ https_example.js
@@ -7,10 +7,21 @@ var options = {
   path: '/'
 };
 
+
 // called by https when the request is made.
-var callback = function() {
+// var callback = function() {
+//   console.log('In response handler callback!');
+var callback = function(response) {
   console.log('In response handler callback!');
+  // console.log('Response: ', response);
+  // called by https when the request is made.
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
+
 
 console.log("I'm about to make the request!");
 
